69bd0223195e587311820aa65790834f45b04508
Prayuj Tuli <prayujtuli@hotmail.com>
2024-05-07 15:13:10 -0400

    added environment variable for music home and directory organization

--- downloader/src/yt_download.rs
+++ downloader/src/yt_download.rs
@@ -1,14 +1,26 @@
+use std::env;
+use std::fs;
+use std::path::Path;
 use std::process::Command;
 
 use crate::spotify_client::Track;
 
 pub fn download_track(track: &Track, url: String) {
+    let music_home = env::var("MUSIC_HOME").expect("MUSIC_HOME not set");
+
+    let path = Path::new(&music_home)
+        .join(&track.album.artists[0].name)
+        .join(&track.album.name);
+
+    fs::create_dir_all(&path).expect("Failed to create directories");
+    let output_path = path.join(format!("{}.mp3", track.name));
+
     let output = Command::new("yt-dlp")
         .arg("-x")
         .arg("--audio-format")
         .arg("mp3")
         .arg("-o")
-        .arg(format!("{}.mp3", track.name))
+        .arg(output_path.to_str().unwrap())
         .arg(url)
         .output()
         .expect("Failed to execute command");
